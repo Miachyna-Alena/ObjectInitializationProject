711853c68b4b8ee5185ce1e3bdbc596c375273f7
Miachyna <vysotskayaalena5@gmail.com>
2022-06-30 20:08:12 +0300

    Add getInformation() method into Student class

--- src/Task01.java
+++ src/Task01.java
@@ -1,9 +1,10 @@
 public class Task01 {
     public static void main(String[] args) {
         Student student = new Student();
-        System.out.println(student.name);
-        System.out.println(student.age);
-        System.out.println(student.gradePointAverage);
-        System.out.println(student.alive);
+//      System.out.println(student.name);
+//      System.out.println(student.age);
+//      System.out.println(student.gradePointAverage);
+//      System.out.println(student.alive);
+        System.out.println(student.getInformation());
     }
 }
--- src/Task02.java
+++ src/Task02.java
@@ -3,25 +3,28 @@ public class Task02 {
         Student student01 = new Student();
         student01.name = "Alex";
         student01.age = 20;
-        System.out.println(student01.name);
-        System.out.println(student01.age);
-        System.out.println(student01.gradePointAverage);
-        System.out.println(student01.alive);
+//      System.out.println(student01.name);
+//      System.out.println(student01.age);
+//      System.out.println(student01.gradePointAverage);
+//      System.out.println(student01.alive);
+        System.out.println(student01.getInformation());
 
         Student student02 = new Student();
         student02.name = "Anna";
         student02.age = 18;
-        System.out.println(student02.name);
-        System.out.println(student02.age);
-        System.out.println(student02.gradePointAverage);
-        System.out.println(student02.alive);
+//      System.out.println(student02.name);
+//      System.out.println(student02.age);
+//      System.out.println(student02.gradePointAverage);
+//      System.out.println(student02.alive);
+        System.out.println(student02.getInformation());
 
         Student student03 = new Student();
         student03.name = "Peter";
         student03.age = 21;
-        System.out.println(student03.name);
-        System.out.println(student03.age);
-        System.out.println(student03.gradePointAverage);
-        System.out.println(student03.alive);
+//      System.out.println(student03.name);
+//      System.out.println(student03.age);
+//      System.out.println(student03.gradePointAverage);
+//      System.out.println(student03.alive);
+        System.out.println(student03.getInformation());
     }
 }
